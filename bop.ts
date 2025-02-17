2d4fdfa5899a65247b14dba5b6e090a8ac59f418
dane <danerieber@gmail.com>
2025-02-17 12:43:06 -0600

    support playing albums

--- bop.ts
+++ bop.ts
@@ -3,14 +3,9 @@ import { parseArgs } from "util";
 const { values } = parseArgs({
   args: Bun.argv,
   options: {
-    beginning: {
+    resume: {
       type: "boolean",
-      short: "b",
-      default: false,
-    },
-    "no-save": {
-      type: "boolean",
-      short: "n",
+      short: "r",
       default: false,
     },
     host: {
@@ -28,13 +23,25 @@ const { values } = parseArgs({
       short: "u",
       default: false,
     },
+    album: {
+      type: "boolean",
+      short: "a",
+      default: false,
+    },
   },
   strict: true,
   allowPositionals: true,
 });
 
+let command = "";
+if (values.album) {
+  command = "fd . --type directory | fzf";
+} else {
+  command = "fzf";
+}
+
 const ssh1 = Bun.spawn(
-  ["ssh", "-t", values.host, "cd " + values.dir + "&& fzf > /tmp/bop"],
+  ["ssh", "-t", values.host, `cd ${values.dir} && ${command} > /tmp/bop`],
   {
     stdin: "inherit",
     stdout: "inherit",
@@ -47,17 +54,35 @@ if (ssh1.exitCode) {
 }
 
 const ssh2 = Bun.spawn(["ssh", values.host, "cat /tmp/bop"]);
-const path = (await new Response(ssh2.stdout).text()).trim();
+const output2 = (await new Response(ssh2.stdout).text()).trim();
 if (ssh2.exitCode) {
   process.exit(ssh1.exitCode);
 }
 
-const mpvArgs = ["sftp://" + values.host + ":" + values.dir + "/" + path];
-if (values.beginning) {
-  mpvArgs.push("--no-resume-playback");
+let paths = [];
+if (values.album) {
+  const ssh3 = Bun.spawn(["ssh", values.host, `ls "${values.dir}/${output2}"`]);
+  const output3 = (await new Response(ssh3.stdout).text()).trim();
+  if (ssh3.exitCode) {
+    process.exit(ssh3.exitCode);
+  }
+  paths = output3.split("\n");
+  for (const [i, path] of paths.entries()) {
+    paths[i] = `${output2}/${path}`;
+  }
+} else {
+  paths = [output2];
 }
-if (!values["no-save"]) {
+
+const mpvArgs = [];
+for (const path of paths) {
+  mpvArgs.push(`sftp://${values.host}:${values.dir}/${path}`);
+}
+
+if (values.resume) {
   mpvArgs.push("--save-position-on-quit");
+} else {
+  mpvArgs.push("--no-resume-playback");
 }
 const mpv = Bun.spawn(["mpv", ...mpvArgs], {
   stdin: "inherit",
